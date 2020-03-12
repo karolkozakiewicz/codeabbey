4a462b7e42f0126af5d7bf2991bef6a7f17702e8
karolkozakiewicz <kaarolek1996@gmail.com>
2020-03-12 16:50:11 +0100

    Added function

--- fahrenheitToCelsius.py
+++ fahrenheitToCelsius.py
@@ -1,8 +1,6 @@
 input = input().split()
 numbers = input[1:]
-answer = []
 def converting(num):
-    answer.append(round((int(number) - 32) * (5 / 9)))
+    print((round((int(number) - 32) * (5 / 9))), '')
 for number in numbers:
     converting(number)
-print(*answer)
